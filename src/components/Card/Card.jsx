0e842bdb056d11eddd55574d4a9a96c6df711779
Carlos Eduardo <121635641+Eduardo-Silva-Carlos@users.noreply.github.com>
2024-05-26 17:21:24 +0000

    Feita algumas modificações

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,10 @@
 import './Card.css'
 export default function Card() {
-    return ( <div className="card">
-        <h1>Java</h1>
-        < img src="https://salvatore.academy/devmon/1_java.png" width="100" />
-    </div>
+    return (
+        <div className="card">
+            <h1>Java</h1>
+            < img src="https://salvatore.academy/devmon/1_java.png" width="100" />
+        </div>
     )
+
 }
