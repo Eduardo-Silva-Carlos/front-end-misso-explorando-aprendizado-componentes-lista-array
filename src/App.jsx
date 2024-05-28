c10d72e1407b67940d34c4730c8eb35fd90c8eb6
Carlos Eduardo <121635641+Eduardo-Silva-Carlos@users.noreply.github.com>
2024-05-28 02:09:18 +0000

    Elemento da lista com estilização adicionado.

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import Card from './components/Card/Card'
 function App() {
   return (
     <>
-      <Card name="Java" imagem="https://salvatore.academy/devmon/1_java.png"/>
-      <Card name= "Kotlin" imagem="https://salvatore.academy/devmon/2_kotlin.png"/>
-      <Card name="Android" imagem="https://salvatore.academy/devmon/3_android.png"/>
+      <div className="cards">
+        <Card name="Java" imagem="https://salvatore.academy/devmon/1_java.png" />
+        <Card name="Kotlin" imagem="https://salvatore.academy/devmon/2_kotlin.png" />
+        <Card name="Android" imagem="https://salvatore.academy/devmon/3_android.png" />
+      </div>
     </>
   )
 }
